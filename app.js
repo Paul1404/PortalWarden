6cca53bba5ae4c5af28abad43b482a8ec11699be
Paul <97813342+Paul1404@users.noreply.github.com>
2023-12-18 23:45:53 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,19 +2,36 @@ const { exec } = require('child_process');
 const Database = require('./db');
 const { initHardware, controlServoAndLEDs } = require('./hardwareControl');
 const readline = require('readline');
+const winston = require('winston');
+const path = require('path');
+
+// Configure Winston logger
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.combine(
+        winston.format.timestamp({
+            format: 'YYYY-MM-DD HH:mm:ss'
+        }),
+        winston.format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`)
+    ),
+    transports: [
+        new winston.transports.File({ filename: path.join(__dirname, 'logs', 'error.log'), level: 'error' }),
+        new winston.transports.File({ filename: path.join(__dirname, 'logs', 'combined.log') })
+    ]
+});
 
 function runPythonScript() {
     const pythonProcess = exec('python3 read_rfid.py', (err, stdout, stderr) => {
         if (err) {
-            console.error('Error executing read_rfid.py:', err);
+            logger.error('Error executing read_rfid.py:', err);
             return;
         }
-        if (stdout) console.log(`Python stdout: ${stdout}`);
-        if (stderr) console.error(`Python stderr: ${stderr}`);
+        if (stdout) logger.info(`Python stdout: ${stdout}`);
+        if (stderr) logger.error(`Python stderr: ${stderr}`);
     });
 
     pythonProcess.on('exit', (code, signal) => {
-        console.log(`Python script exited with code ${code} and signal ${signal}`);
+        logger.info(`Python script exited with code ${code} and signal ${signal}`);
     });
 }
 
@@ -24,7 +41,7 @@ function setupKeypressListener(db) {
 
     process.stdin.on('keypress', async (str, key) => {
         if (key.ctrl && key.name === 'x') {
-            console.log('Ctrl+X pressed. Shutting down...');
+            logger.info('Ctrl+X pressed. Initiating shutdown...');
             await gracefulShutdown(db);
         }
     });
@@ -32,7 +49,7 @@ function setupKeypressListener(db) {
 
 async function gracefulShutdown(db) {
     await db.close();
-    console.log('Database connection closed.');
+    logger.info('Database connection closed.');
     process.exit(0);
 }
 
@@ -40,7 +57,7 @@ async function main() {
     const db = new Database();
     initHardware();
 
-    console.log('Node.js program started.');
+    logger.info('Node.js program started.');
 
     // Run the Python script
     runPythonScript();
