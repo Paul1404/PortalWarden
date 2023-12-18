48b546db4793ed972adc8de40b5a67b55253b05b
Paul <97813342+Paul1404@users.noreply.github.com>
2023-12-18 23:52:41 +0100

    Docstring Documentation for read_rfid.py

--- read_rfid.py
+++ read_rfid.py
@@ -1,42 +1,74 @@
-# Description: This script reads the RFID card and writes the id and current timestamp to the SQLite database. File: read_rfid.py
-
-import RPi.GPIO as GPIO  # type: ignore
-from mfrc522 import SimpleMFRC522  # type: ignore
+import RPi.GPIO as GPIO
+from mfrc522 import SimpleMFRC522
 import time
 import logging
 import sqlite3
 from datetime import datetime
 
-logging.basicConfig(filename='rfid_reader.log', level=logging.INFO)
-logger = logging.getLogger(__name__)
+def configure_logging():
+    """
+    Configures the logging for the application.
+    Logs are written to 'rfid_reader.log'.
+    """
+    logging.basicConfig(filename='rfid_reader.log', level=logging.INFO)
+    return logging.getLogger(__name__)
 
-reader = SimpleMFRC522()
+logger = configure_logging()
 
-# Connect to the SQLite database
-conn = sqlite3.connect('rfid_database.db')
-c = conn.cursor()
+class RFIDReader:
+    """
+    A class to handle RFID reading and logging RFID data to a SQLite database.
+    """
 
-# Create table if it doesn't exist
-c.execute('''
-    CREATE TABLE IF NOT EXISTS logins (
-        id INTEGER PRIMARY KEY,
-        timestamp TEXT
-    )
-''')
+    def __init__(self):
+        """
+        Initializes the RFID reader and establishes a database connection.
+        """
+        self.reader = SimpleMFRC522()
+        self.conn = sqlite3.connect('rfid_database.db')
+        self.cursor = self.conn.cursor()
+        self.setup_database()
 
-try:
-    while True:
-        logger.info("Ready to read RFID card")
+    def setup_database(self):
+        """
+        Sets up the database table for storing RFID logins.
+        """
+        self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS logins (
+                id INTEGER PRIMARY KEY,
+                timestamp TEXT
+            )
+        ''')
+
+    def read_rfid(self):
+        """
+        Continuously reads from the RFID reader and logs the data.
+        """
         try:
-            id, text = reader.read()
-            logger.info(id)
-            # Insert the id and current timestamp into the database
-            c.execute("INSERT INTO logins VALUES (?, ?)", (id, datetime.now()))
-            conn.commit()
-            time.sleep(1)  # delay to prevent immediate subsequent reads
-        except Exception as e:
-            logger.error(f"An error occurred: {e}")
-finally:
-    # Close the database connection
-    conn.close()
-    GPIO.cleanup()
+            while True:
+                logger.info("Ready to read RFID card")
+                try:
+                    id, text = self.reader.read()
+                    logger.info(f"RFID ID: {id}")
+
+                    # Insert the ID and current timestamp into the database
+                    self.cursor.execute("INSERT INTO logins VALUES (?, ?)", (id, datetime.now()))
+                    self.conn.commit()
+
+                    # Sleep for 1 second to prevent immediate subsequent reads
+                    time.sleep(1)
+                except Exception as e:
+                    logger.error(f"An error occurred: {e}")
+        finally:
+            self.close()
+
+    def close(self):
+        """
+        Closes the database connection and cleans up GPIO.
+        """
+        self.conn.close()
+        GPIO.cleanup()
+
+if __name__ == "__main__":
+    reader = RFIDReader()
+    reader.read_rfid()
