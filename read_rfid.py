2625ef71c0407a4ede32371d41071df35aaf8c0a
Paul <97813342+Paul1404@users.noreply.github.com>
2023-12-18 20:38:10 +0100

    Add SQLite database functionality for logging RFID card reads

--- read_rfid.py
+++ read_rfid.py
@@ -2,11 +2,25 @@ import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
 import time
 import logging
+import sqlite3
+from datetime import datetime
 
 logging.basicConfig(filename='rfid_reader.log', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 reader = SimpleMFRC522()
+
+# Connect to the SQLite database
+conn = sqlite3.connect('rfid_database.db')
+c = conn.cursor()
+
+# Create table if it doesn't exist
+c.execute('''
+    CREATE TABLE IF NOT EXISTS logins (
+        id INTEGER PRIMARY KEY,
+        timestamp TEXT
+    )
+''')
 
 try:
     while True:
@@ -14,8 +28,13 @@ try:
         try:
             id, text = reader.read()
             logger.info(id)
+            # Insert the id and current timestamp into the database
+            c.execute("INSERT INTO logins VALUES (?, ?)", (id, datetime.now()))
+            conn.commit()
             time.sleep(1)  # delay to prevent immediate subsequent reads
         except Exception as e:
             logger.error(f"An error occurred: {e}")
 finally:
+    # Close the database connection
+    conn.close()
     GPIO.cleanup()
