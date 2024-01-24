c49c9fc3a55590633a02d2105002cb213728062c
Paul1404 <paul.dresch@untereuerheim.com>
2024-01-24 22:07:45 +0100

    Update both scripts

--- tools/setup-db.sh
+++ tools/setup-db.sh
@@ -1,48 +1,57 @@
 #!/bin/bash
 
+# Logging function
+log() {
+    level=$1
+    shift
+    msg="$@"
+    timestamp=$(date '+%Y-%m-%d %H:%M:%S')
+    echo "[$timestamp] $level: $msg" | tee -a "$LOGFILE"
+}
+
 # Log directory and file setup
 LOGDIR="$BASEDIR/logs/setup"
 mkdir -p "$LOGDIR"
 LOGFILE="$LOGDIR/setup-db_$(date +%Y%m%d_%H%M%S).log"
 
-# Run Prisma migrations to set up the database
-echo "Running Prisma migrations to set up the database..." | tee -a $LOGFILE
+log "INFO" "Running Prisma migrations to set up the database..."
 cd ..  # Navigate to the directory containing your Prisma schema
 prisma migrate deploy | tee -a $LOGFILE
 
-echo "Database setup complete." | tee -a $LOGFILE
+log "INFO" "Database setup complete."
 
 # Ask the user if they want to create a new user
+log "INFO" "Asking user about creating a new user..."
 read -p "Do you want to create a new user? (y/n) " answer
 case $answer in
     [Yy]* ) 
-        echo "Creating new user..." | tee -a $LOGFILE
+        log "INFO" "Creating new user..."
         node ./tools/addUser.js | tee -a $LOGFILE
         ;;
     [Nn]* ) 
-        echo "Skipping user creation." | tee -a $LOGFILE
+        log "INFO" "Skipping user creation."
         ;;
     * ) 
-        echo "Please answer yes or no." | tee -a $LOGFILE
+        log "WARNING" "Invalid input. Please answer yes (y) or no (n)."
         ;;
 esac
 
 # Ask the user if they want to enable SPI
+log "INFO" "Asking user about enabling SPI on the Raspberry Pi..."
 read -p "Do you want to enable SPI on the Raspberry Pi? (y/n) " spi_answer
 case $spi_answer in
     [Yy]* ) 
-        echo "Enabling SPI..." | tee -a $LOGFILE
+        log "INFO" "Enabling SPI..."
         ./enable-spi.sh | tee -a $LOGFILE
-        echo "SPI enabled." | tee -a $LOGFILE
+        log "INFO" "SPI enabled."
         ;;
     [Nn]* ) 
-        echo "Skipping SPI enable." | tee -a $LOGFILE
+        log "INFO" "Skipping SPI enable."
         ;;
     * ) 
-        echo "Please answer yes (y) or no (n)." | tee -a $LOGFILE
+        log "WARNING" "Invalid input. Please answer yes (y) or no (n)."
         ;;
 esac
 
-echo "Finished! You can run the webserver with: npm run web" | tee -a $LOGFILE
-
+log "INFO" "Finished setup. Webserver can be run with: npm run web"
 exit
--- tools/setup-main.sh
+++ tools/setup-main.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-# Determine the script's current directory
-SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+# Logging function
+log() {
+    level=$1
+    shift
+    msg="$@"
+    timestamp=$(date '+%Y-%m-%d %H:%M:%S')
+    echo "[$timestamp] $level: $msg" | tee -a "$LOGFILE"
+}
 
-# Determine the base directory of the project
+# Determine the script's current directory and base directory
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 BASEDIR=$(dirname "$SCRIPT_DIR")
 
 # Log directory and file setup
@@ -11,7 +18,7 @@ LOGDIR="$BASEDIR/logs/setup"
 mkdir -p "$LOGDIR"
 LOGFILE="$LOGDIR/setup-main_$(date +%Y%m%d_%H%M%S).log"
 
-echo "Starting setup script" | tee -a $LOGFILE
+log "INFO" "Starting setup script"
 
 # Function to check if a command exists
 command_exists() {
@@ -24,8 +31,10 @@ install_command() {
     package=$2
 
     if ! command_exists "$command"; then
-        echo "Installing $command..." | tee -a $LOGFILE
+        log "INFO" "Installing $command..."
         sudo apt-get install -y "$package" | tee -a $LOGFILE
+    else
+        log "INFO" "$command already installed."
     fi
 }
 
@@ -37,7 +46,7 @@ read_password() {
     trap 'stty echo' EXIT
 
     password=""
-    while IFS= read -p "$prompt" -r -s -n 1 char; do
+    while IFS= read -r -s -n 1 char; do
         if [[ $char == $'\0' ]]; then
             break
         fi
@@ -51,89 +60,73 @@ read_password() {
 }
 
 # Check and install required commands
-install_command argon2 argon2 | tee -a $LOGFILE
-install_command awk gawk | tee -a $LOGFILE
+install_command argon2 argon2
+install_command awk gawk
 
 # Handling the .env file
 ENV_FILE="$BASEDIR/.env"
 
-# Check if .env file exists in the parent directory, if not, copy from sample in current directory
 if [ ! -f "$ENV_FILE" ]; then
     if [ -f "$SCRIPT_DIR/.env.sample" ]; then
         cp "$SCRIPT_DIR/.env.sample" "$ENV_FILE"
-        echo "Copied .env.sample from tools to .env in the parent directory" | tee -a $LOGFILE
+        log "INFO" "Copied .env.sample from tools to .env in the parent directory"
     else
-        echo "The .env.sample file does not exist in the tools directory" | tee -a $LOGFILE
+        log "ERROR" "The .env.sample file does not exist in the tools directory"
         exit 1
     fi
 fi
+
 # Generate a more secure random string for SESSION_SECRET
 RANDOM_STRING=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 32)
-echo 'Generated a secure RANDOM_STRING for SESSION_SECRET' | tee -a $LOGFILE
+log "INFO" "Generated a secure RANDOM_STRING for SESSION_SECRET"
 
-# Check if SESSION_SECRET already exists in .env
+# Update SESSION_SECRET in .env
 if grep -q "SESSION_SECRET=" "$ENV_FILE"; then
-    # Replace the existing SESSION_SECRET
     sed -i "/SESSION_SECRET=/c\SESSION_SECRET=$RANDOM_STRING" "$ENV_FILE"
-    echo "Replaced existing SESSION_SECRET in .env" | tee -a $LOGFILE
+    log "INFO" "Replaced existing SESSION_SECRET in .env"
 else
-    # Ensure there's a newline at the end of .env
     tail -c1 "$ENV_FILE" | read -r _ || echo >> "$ENV_FILE"
-
-    # Then append SESSION_SECRET
     echo "SESSION_SECRET=$RANDOM_STRING" >> "$ENV_FILE"
-    echo "Appended SESSION_SECRET to .env" | tee -a $LOGFILE
+    log "INFO" "Appended SESSION_SECRET to .env"
 fi
 
-# Ask for the PostgreSQL password without displaying input
+# Ask for the PostgreSQL password
 echo -n "Enter the PostgreSQL password: "
 IFS= read -rs POSTGRES_PASSWORD
 echo
-echo "Setting PostgreSQL password" | tee -a $LOGFILE
+log "INFO" "Setting PostgreSQL password"
 
-
-# Update the .env file with the PostgreSQL password
+# Update POSTGRES_PASSWORD in .env
 if grep -q "POSTGRES_PASSWORD=" "$ENV_FILE"; then
-    # Replace the existing POSTGRES_PASSWORD
     sed -i "/POSTGRES_PASSWORD=/c\POSTGRES_PASSWORD=$POSTGRES_PASSWORD" "$ENV_FILE"
-    echo "Updated existing POSTGRES_PASSWORD in .env" | tee -a $LOGFILE
+    log "INFO" "Updated existing POSTGRES_PASSWORD in .env"
 else
-    # Ensure there's a newline at the end of .env
     tail -c1 "$ENV_FILE" | read -r _ || echo >> "$ENV_FILE"
-
-    # Then append POSTGRES_PASSWORD
     echo "POSTGRES_PASSWORD=$POSTGRES_PASSWORD" >> "$ENV_FILE"
-    echo "Appended POSTGRES_PASSWORD to .env" | tee -a $LOGFILE
-
+    log "INFO" "Appended POSTGRES_PASSWORD to .env"
 fi
 
-# Update the DATABASE_URL with the new password
+# Update DATABASE_URL in .env
 if grep -q "DATABASE_URL=" "$ENV_FILE"; then
-    # Replace the password in DATABASE_URL
     sed -i "s/DATABASE_URL=\"postgresql:\/\/admin:.*@/DATABASE_URL=\"postgresql:\/\/admin:$POSTGRES_PASSWORD@/" "$ENV_FILE"
-    echo "Updated DATABASE_URL in .env" | tee -a $LOGFILE
+    log "INFO" "Updated DATABASE_URL in .env"
 fi
 
-# Navigate to the directory where docker-compose.yml is located
+# Docker Compose Actions
 cd "$BASEDIR"
-
-# Ask the user if they want to start the Docker containers
-echo "Do you want to start the Database-Docker container now? (Recommended) (y/n)" | tee -a $LOGFILE
+log "INFO" "Asking user to start Docker containers"
+echo "Do you want to start the Database-Docker container now? (Recommended) (y/n)"
 read -r start_containers
 
 if [[ "$start_containers" =~ ^[Yy]$ ]]; then
-    # Run Docker Compose to start the containers
-    echo "Starting Docker container with Docker Compose..." | tee -a $LOGFILE
+    log "INFO" "Starting Docker container with Docker Compose"
     docker-compose -p rpi-rfid -f docker/postgres-compose.yml up -d
-    echo "Docker containers started." | tee -a $LOGFILE
+    log "INFO" "Docker containers started"
 else
-    echo "Docker container not started. You can start them later by running:" | tee -a $LOGFILE
-    echo "docker-compose -p rpi-rfid -f postgres-compose.yml up -d" | tee -a $LOGFILE
-    echo "The compose file is located under the docker directory in the root folder" | tee -a $LOGFILE
+    log "INFO" "Docker container not started. Informed user about manual start."
+    echo "You can start the container with this command below the compose file is located in the docker directory make sure to point to it when running this command."
+    echo "docker-compose -p rpi-rfid -f postgres-compose.yml up -d"
 fi
 
-
-
-echo "Setup script completed" | tee -a $LOGFILE
-
+log "INFO" "Setup script completed"
 exit
