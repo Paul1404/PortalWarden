48210adaf0b42fe254a5756f81226e0c3ea88778
Paul1404 <paul.dresch@untereuerheim.com>
2024-01-25 22:00:25 +0100

    migrate config.js to env

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    servoPin: 12,
-    greenLedPin: 3,
-    redLedPin: 5,
-    pulseWidth: 1500
-};
--- hardwareControl.js
+++ hardwareControl.js
@@ -1,36 +1,26 @@
+require('dotenv').config();
 const rpio = require('rpio');
-const config = require('./config');
 
-/**
- * Initializes the hardware components by setting the GPIO pins.
- * This includes setting up the servo motor and LED pins.
- */
 function initHardware() {
-    rpio.open(config.servoPin, rpio.OUTPUT, rpio.LOW);
-    rpio.open(config.greenLedPin, rpio.OUTPUT, rpio.LOW);
-    rpio.open(config.redLedPin, rpio.OUTPUT, rpio.LOW);
+    rpio.open(parseInt(process.env.SERVO_PIN), rpio.OUTPUT, rpio.LOW);
+    rpio.open(parseInt(process.env.GREEN_LED_PIN), rpio.OUTPUT, rpio.LOW);
+    rpio.open(parseInt(process.env.RED_LED_PIN), rpio.OUTPUT, rpio.LOW);
 }
 
-/**
- * Controls the servo motor and LEDs based on the RFID value.
- * Moves the servo motor and turns on the green LED if RFID is valid,
- * otherwise turns on the red LED.
- * @param {boolean} rfidValue - The value indicating RFID tag validity.
- */
 function controlServoAndLEDs(rfidValue) {
     // Move the servo motor
-    rpio.write(config.servoPin, rpio.HIGH);
-    rpio.usleep(config.pulseWidth);
-    rpio.write(config.servoPin, rpio.LOW);
+    rpio.write(parseInt(process.env.SERVO_PIN), rpio.HIGH);
+    rpio.usleep(parseInt(process.env.PULSE_WIDTH));
+    rpio.write(parseInt(process.env.SERVO_PIN), rpio.LOW);
     rpio.msleep(20); // Wait for 20 milliseconds
 
     // Control LEDs based on RFID value
     if (rfidValue) {
-        rpio.write(config.greenLedPin, rpio.HIGH); // Turn on green LED
-        rpio.write(config.redLedPin, rpio.LOW); // Turn off red LED
+        rpio.write(parseInt(process.env.GREEN_LED_PIN), rpio.HIGH); // Turn on green LED
+        rpio.write(parseInt(process.env.RED_LED_PIN), rpio.LOW); // Turn off red LED
     } else {
-        rpio.write(config.greenLedPin, rpio.LOW); // Turn off green LED
-        rpio.write(config.redLedPin, rpio.HIGH); // Turn on red LED
+        rpio.write(parseInt(process.env.GREEN_LED_PIN), rpio.LOW); // Turn off green LED
+        rpio.write(parseInt(process.env.RED_LED_PIN), rpio.HIGH); // Turn on red LED
     }
 }
 
